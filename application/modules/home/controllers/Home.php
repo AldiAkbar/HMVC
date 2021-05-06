f8afe59a027438f75b44cebe4b244573b9c5d763
AldiAkbar <72376105+AldiAkbar@users.noreply.github.com>
2021-05-07 03:16:21 +0700

    upload file

---
+++ application/modules/home/controllers/Home.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Home extends CI_Controller {
+
+	public function index()
+	{
+		echo 'OK!';
+	}
+
+	public function halo()
+	{
+		echo 'PHP Codeigniter with HMVC';
+	}
+	
+}
+
+/* End of file Home.php */
+/* Location: ./application/modules/home/controllers/Home.php */
